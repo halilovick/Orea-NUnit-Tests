db04bbf63b19601f0889ff99e095b771526286c0
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-01-16 22:09:52 +0100

    Added SlanjePorukeTest

--- NUnit Selenium VVS/OdjavaTest.cs
+++ NUnit Selenium VVS/OdjavaTest.cs
@@ -11,47 +11,30 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
-using SeleniumExtras.WaitHelpers;
-
 [TestFixture]
-public class TestiranjeOdjave
+public class OdjavaTest
 {
     private IWebDriver driver;
     public IDictionary<string, object> vars { get; private set; }
     private IJavaScriptExecutor js;
-
     [SetUp]
     public void SetUp()
     {
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
-
-        // Setting implicit wait to 10 seconds
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
     }
-
     [TearDown]
     protected void TearDown()
     {
         driver.Quit();
     }
-
     [Test]
-    public void testTestiranjeslanjaporuke()
+    public void testTestiranjeodjavesprofila()
     {
         driver.Navigate().GoToUrl("https://www.oreabazaar.com/");
         driver.Manage().Window.Size = new System.Drawing.Size(786, 823);
-
-        // Waiting for the cookie consent banner to be present before clicking
-        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".cc-15e7")));
-
-        driver.FindElement(By.CssSelector(".cc-15e7")).Click();
-
-        // Adding a sleep for demonstration purposes, consider replacing it with a more specific wait
-        Thread.Sleep(2000);
-
-        js.ExecuteScript("window.scrollTo(0,700)");
+        driver.FindElement(By.CssSelector(".mt-2 > img")).Click();
+        driver.FindElement(By.CssSelector(".border:nth-child(1) > .cursor-pointer")).Click();
     }
 }
